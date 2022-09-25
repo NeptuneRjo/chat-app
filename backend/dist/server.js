4d3626148d7a538c8ab569a16868f74c5a8fe7be
NeptuneRjo <rjo9000@outlook.com>
2022-09-25 02:11:07 -0400

    compile

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,18 +4,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const socket_io_1 = require("socket.io");
 require("./config/mongoConfig");
 require("dotenv/config");
 const mongoose_1 = require("mongoose");
+const http_1 = require("http");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 4000;
+const httpServer = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(httpServer, {});
 /* <-- Middleware --> */
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 /* <-- Routes --> */
 /* <-- Server --> */
 mongoose_1.connection.on('connected', () => {
-    app.listen(port, () => {
-        console.log('Connected to DB and listening on port:', port);
+    httpServer.listen(port, () => {
+        console.log('Connect to DB and listening on port:', port);
     });
+});
+/* Web Sockets */
+io.on('connection', (socket) => {
+    console.log('Made socket connection', socket.id);
+});
+io.engine.on('connection_error', (err) => {
+    const { req, code, message, context } = err;
+    console.log(req);
+    console.log(code);
+    console.log(message);
+    console.log(context);
 });
