effd0883fa30880ca47a9743527506ae5af893dc
NeptuneRjo <rjo9000@outlook.com>
2022-09-25 18:09:01 -0400

    setup basic routes for auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -5,3 +5,5 @@ const router = Router()
 router.route('/signup').post((req, res) => res.send('POST signup'))
 router.route('/login').post((req, res) => res.send('POST login'))
 router.route('/logout').post((req, res) => res.send('POST logout'))
+
+export default router
