d1c35e8252032a3a6b95a4baea6f6851f6388679
NeptuneRjo <rjo9000@outlook.com>
2022-09-26 18:59:03 -0400

    set up auth routes properly

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,48 +1,36 @@
 import { Router } from 'express'
 import passport from 'passport'
+import { Request, Response, NextFunction } from 'express'
 
 const router = Router()
 
-const isLoggedIn = (req: any, res: any, next: any) => {
-	console.log(req.user)
+const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
 	req.user ? next() : res.sendStatus(401)
 }
 
-router.route('/signup').post((req, res) => res.send('POST signup'))
-router.route('/login').post((req, res) => res.send('POST login'))
-router.route('/logout').post((req, res) => res.send('POST logout'))
-
-router
-	.route('/')
-	.get((req, res) =>
-		res.send('<a href="/auth/google">Authenticate with Google</a>')
-	)
-router.route('/protected').get(isLoggedIn, (req, res) => {
-	res.send(`Hello!`)
-})
-
+// Login and/or Signup
 router.get(
-	'/auth/google',
+	'/google',
 	passport.authenticate('google', { scope: ['email', 'profile'] })
 )
-
-router.get(
-	'/google/callback',
-	passport.authenticate('google', {
-		successRedirect: '/protected',
-		failureRedirect: '/auth/failure',
-	})
-)
-
-router.get('/auth/failure', (req, res) => {
-	res.send('something went wrong')
-})
 
 router.get('/logout', (req, res, next) => {
 	req.session.destroy(() => {
 		console.log(req.user)
 		res.send('Goodbye')
 	})
+})
+
+router.get('/google/callback', passport.authenticate('google'), (req, res) => {
+	if (req.user) {
+		res.send(200).json({ data: req.user })
+	} else {
+		res.send(400).json({ error: 'Unable to log in at this time' })
+	}
+})
+
+router.get('/failure', (req, res) => {
+	res.send('something went wrong')
 })
 
 export default router
