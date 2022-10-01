e1c57c57eed825886a565766feb303dbbfc4854e
NeptuneRjo <rjo9000@outlook.com>
2022-10-01 14:52:23 -0400

    add fetch for current user and add props to menu and room

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Menu, Room } from './Containers'
 import 'bootswatch/dist/lux/bootstrap.min.css'
+import { getUser } from './Api'
+import { UserInterface } from './types'
 
 import './App.css'
 
 function App() {
+	const [error, setError] = useState<unknown | null>(null)
+	const [user, setUser] = useState<undefined | UserInterface>(undefined)
+
+	useEffect(() => {
+		;(async () => {
+			const response = await getUser()
+			const json = await response.json()
+
+			if (!response.ok) {
+				setError(json.error)
+			} else {
+				setUser(json.data)
+			}
+		})()
+	}, [])
+
 	return (
 		<HashRouter>
 			<div className='app-main bg-dark'>
-				<Menu />
+				<Menu user={user} />
 				<Routes>
-					<Route path='/' element={<Room />} />
+					<Route path='/' element={<Room user={user} />} />
 				</Routes>
 			</div>
 		</HashRouter>
