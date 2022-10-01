1a6a3383a3ccbcba8f081c3be6221a30f39a97f6
NeptuneRjo <rjo9000@outlook.com>
2022-10-01 18:50:03 -0400

    add patch to room identifier

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -4,15 +4,11 @@ import {
 	post_new_message,
 	post_new_room,
 	delete_room,
-	post_new_member,
 } from '../controllers/chatControllers'
 
 const router = Router()
 
-router.route('/:id').get(get_room).delete(delete_room)
-
-router.route('/:id/new-message').patch(post_new_message)
-router.route('/:id/new-user').patch(post_new_member)
+router.route('/:id').get(get_room).delete(delete_room).patch(post_new_message)
 
 router.route('/new-room').post(post_new_room)
 
