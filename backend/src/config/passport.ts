bddb6042d072baee43057f0880a2da5e77db0e0e
NeptuneRjo <rjo9000@outlook.com>
2022-10-03 15:20:56 -0400

    update passport callback url

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -11,7 +11,7 @@ passport.use(
 		{
 			clientID: GOOGLE_CLIENT_ID as string,
 			clientSecret: GOOGLE_CLIENT_SECRET as string,
-			callbackURL: 'https://chat-app-0iem.onrender.com/auth/google/callback',
+			callbackURL: 'http://localhost:3000/auth/google/callback',
 			passReqToCallback: true,
 		},
 		function (
