6429a200605dfe2f9ca7f1c2c6467c0df2d89d9d
NeptuneRjo <rjo9000@outlook.com>
2022-10-03 23:29:39 -0400

    update

--- frontend/src/Api/Auth/getUser.ts
+++ frontend/src/Api/Auth/getUser.ts
@@ -2,8 +2,11 @@ export const getUser = async (): Promise<Response> => {
 	const response: Response = await fetch(
 		`${process.env.REACT_APP_API_URL}/auth/login`,
 		{
+			method: 'GET',
 			headers: {
+				Accept: 'application/json',
 				'Content-type': 'application/json; charset=UTF-8',
+				'Access-Control-Allow-Credentials': 'true',
 			},
 			credentials: 'include',
 		}
