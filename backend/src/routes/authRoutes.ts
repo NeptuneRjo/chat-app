b86dfe63ecb16e5facfdeeea4764a95027067717
NeptuneRjo <rjo9000@outlook.com>
2022-10-04 17:12:32 -0400

    update

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -22,7 +22,7 @@ router.get('/logout', (req, res, next) => {
 router.get(
 	'/google/callback',
 	passport.authenticate('google', {
-		successRedirect: process.env.REDIRECT_URL as string,
+		successRedirect: process.env.REDIRECT_URL,
 		failureRedirect: '/auth/failure',
 	})
 )
