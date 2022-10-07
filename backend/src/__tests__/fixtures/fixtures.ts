13e258749ebdcf063cdb804d9a57acbab52b4dc3
NeptuneRjo <rjo9000@outlook.com>
2022-10-07 19:58:51 -0400

    create fixtures for chat model testing

---
+++ backend/src/__tests__/fixtures/fixtures.ts
@@ -0,0 +1,19 @@
+export const fakeMessage = {
+	handle: 'Neptune',
+	message: 'Hello World!',
+}
+
+export const fakeMessageFail = {
+	handle: 1,
+	message: 2,
+}
+
+export const fakeRoom = {
+	messages: [],
+	roomId: 'room-1',
+}
+
+export const fakeRoomFail = {
+	messages: [1],
+	roomId: 1,
+}
