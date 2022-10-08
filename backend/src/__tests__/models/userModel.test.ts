e5fd7b876626d98c4efe920f1a4d2e51c3cacf40
NeptuneRjo <rjo9000@outlook.com>
2022-10-07 23:49:43 -0400

    add tests for user model

---
+++ backend/src/__tests__/models/userModel.test.ts
@@ -0,0 +1,53 @@
+import { User } from '../../models'
+import { fakeUser, fakeUserFail } from '../fixtures/fixtures'
+import {
+	initializeMongoServer,
+	deinitializeMongoServer,
+	dropCollections,
+} from '../config/mongoConfigTest'
+
+import 'jest'
+
+describe('User Model', () => {
+	beforeAll(async () => {
+		const mongoServer = await initializeMongoServer()
+	})
+
+	afterAll(async () => {
+		await deinitializeMongoServer()
+	})
+
+	afterEach(async () => {
+		await dropCollections()
+	})
+
+	it('should create the user item successfully', async () => {
+		const newUser = await User.create(fakeUser)
+		const { googleId, picture, displayName } = newUser
+
+		expect(googleId).toBe(fakeUser.googleId)
+		expect(picture).toBe(fakeUser.picture)
+		expect(displayName).toBe(fakeUser.displayName)
+	})
+
+	it('should fail to create the user item with no content', async () => {
+		let error = null
+		try {
+			const newUser = new User()
+			await newUser.validate()
+		} catch (err) {
+			error = err
+		}
+
+		expect(error).not.toBeNull()
+	})
+
+	it('should fail to create a user with invalid field types', async () => {
+		try {
+			const newUser = new User(fakeUserFail)
+			await newUser.validate()
+		} catch (err) {
+			expect(err).not.toBeNull()
+		}
+	})
+})
