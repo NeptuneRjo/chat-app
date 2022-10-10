81ce9c4a97895728bf3283025e4b9b7f0df46c2b
NeptuneRjo <rjo9000@outlook.com>
2022-10-10 17:35:16 -0400

    add logout and set function to utils

--- frontend/src/Global/utils.ts
+++ frontend/src/Global/utils.ts
@@ -1,11 +1,18 @@
 import Cookies from 'js-cookie'
-import { getUser } from '../Api'
+import { getUser, logoutUser } from '../Api'
 
 //
 export const getAndSetUser = async (token: string) => {
 	Cookies.remove('x-auth-header')
 
 	const response = await getUser(token)
+	const { data, error } = await response.json()
+
+	return { data, error }
+}
+
+export const logoutAndSetUser = async () => {
+	const response = await logoutUser()
 	const { data, error } = await response.json()
 
 	return { data, error }
