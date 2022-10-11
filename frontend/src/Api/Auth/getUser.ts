dad5734c954457ca17eaf2f60b7bc1feebd42f15
NeptuneRjo <rjo9000@outlook.com>
2022-10-11 17:46:20 -0400

    update

--- frontend/src/Api/Auth/getUser.ts
+++ frontend/src/Api/Auth/getUser.ts
@@ -1,12 +1,15 @@
 export const getUser = async (token: string): Promise<Response> => {
-	const response: Response = await fetch(`/auth/login`, {
-		method: 'GET',
-		headers: {
-			'Content-type': 'application/json; charset=UTF-8',
-			'X-auth-token': token,
-		},
-		credentials: 'include',
-	})
+	const response: Response = await fetch(
+		`${process.env.REACT_APP_API_URL}/auth/login`,
+		{
+			method: 'GET',
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+				'X-auth-token': token,
+			},
+			credentials: 'include',
+		}
+	)
 
 	return response
 }
