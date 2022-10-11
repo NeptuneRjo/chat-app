cec58b2b8865645290ed4716fe8ce15fd14a8603
NeptuneRjo <rjo9000@outlook.com>
2022-10-10 23:37:54 -0400

    replace util functions with general getAndSet function

--- frontend/src/Global/utils.ts
+++ frontend/src/Global/utils.ts
@@ -1,38 +1,9 @@
-import Cookies from 'js-cookie'
-import { getUser, logoutUser, getRoom, newMessage } from '../Api'
-
-//
-export const getAndSetUser = async (token: string) => {
-	Cookies.remove('x-auth-header')
-
-	const response = await getUser(token)
-	const { data, error } = await response.json()
-
-	return { data, error }
-}
-
-export const logoutAndSetUser = async () => {
-	const response = await logoutUser()
-	const { data, error } = await response.json()
-
-	return { data, error }
-}
-
-export const getAndSetRoom = async (id: string) => {
-	const response = await getRoom(id)
-	const { data, error } = await response.json()
-
-	return { data, error }
-}
-
-export const getAndSetNewMessage = async (
-	id: string,
-	message: {
-		handle: string
-		message: string
-	}
+export const getAndSet = async (
+	api: Function,
+	id: string | undefined = undefined,
+	body: string | object | undefined = undefined
 ) => {
-	const response = await newMessage(id, message)
+	const response = await api(id, body)
 	const { data, error } = await response.json()
 
 	return { data, error }
