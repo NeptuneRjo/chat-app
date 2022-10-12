0d2a6c38e481964924db76552f1f7ac145fe8f34
NeptuneRjo <rjo9000@outlook.com>
2022-10-11 23:43:32 -0400

    update

--- frontend/src/Api/Auth/getUser.ts
+++ frontend/src/Api/Auth/getUser.ts
@@ -6,7 +6,7 @@ export const getUser = async (): Promise<Response> => {
 			headers: {
 				'Content-type': 'application/json; charset=UTF-8',
 			},
-			credentials: 'same-origin',
+			credentials: 'include',
 		}
 	)
 
