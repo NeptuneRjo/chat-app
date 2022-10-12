9e3f33d879c6b742b79a6e3c313ee75c9bb39231
NeptuneRjo <rjo9000@outlook.com>
2022-10-12 16:26:32 -0400

    update

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -44,7 +44,7 @@ app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, express_session_1.default)({
     secret: process.env.EXPRESS_SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
 }));
 app.use(passport_1.default.session());
 app.use(passport_1.default.initialize());
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -48,7 +48,7 @@ app.use(
 	session({
 		secret: process.env.EXPRESS_SESSION_SECRET as string,
 		resave: false,
-		saveUninitialized: true,
+		saveUninitialized: false,
 	})
 )
 
