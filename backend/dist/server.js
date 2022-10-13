cfb11ceee311e891537e4a3a1205fc99b24b9dce
NeptuneRjo <rjo9000@outlook.com>
2022-10-13 17:34:59 -0400

    update

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -30,7 +30,7 @@ const io = new socket_io_1.Server(httpServer, {
 });
 /* <-- Middleware --> */
 app.use((0, cors_1.default)({
-    origin: ['http://localhost:3000', 'https://chat-app-0iem.onrender.com'],
+    origin: ['http://localhost:3000', 'https://harmony-45tv.onrender.com'],
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE', 'PATCH'],
     credentials: true,
 }));
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,7 +30,7 @@ const io = new Server(httpServer, {
 
 app.use(
 	cors({
-		origin: ['http://localhost:3000', 'https://chat-app-0iem.onrender.com'],
+		origin: ['http://localhost:3000', 'https://harmony-45tv.onrender.com'],
 		methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE', 'PATCH'],
 		credentials: true,
 	})
