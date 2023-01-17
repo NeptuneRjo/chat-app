8145850fb83da0e5fda33a80ebd588c82351b64e
NeptuneRjo <rjo9000@outlook.com>
2023-01-17 15:23:43 -0500

    update

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -60,7 +60,7 @@ app.use((0, express_session_1.default)({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        sameSite: 'none',
+        // sameSite: 'none',
         secure: true,
         domain: 'chat-app-0iem.onrender.com',
     },
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -69,7 +69,7 @@ app.use(
 		resave: false,
 		saveUninitialized: true,
 		cookie: {
-			sameSite: 'none',
+			// sameSite: 'none',
 			secure: true,
 			domain: 'chat-app-0iem.onrender.com',
 		},
