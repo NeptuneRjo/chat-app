d9dc629d453953483f40b2eb21ec69eae988c2ca
NeptuneRjo <rjo9000@outlook.com>
2023-01-20 16:13:49 -0500

    restructure user model

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,11 +1,52 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model, Model } from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
-const userModel = new Schema({
-	googleId: { type: String, required: true },
-	picture: { type: String, required: true },
-	displayName: { type: String, required: true },
+import 'dotenv/config'
+import { RoomType } from '../types'
+
+interface IUser {
+	username: string
+	password: string
+	rooms: RoomType
+}
+
+interface IUserMethods {
+	generateJWT(): string
+}
+
+type UserModel = Model<IUser, {}, IUserMethods>
+
+const userSchema = new Schema<IUser, UserModel, IUserMethods>({
+	username: { type: String, required: true },
+	password: { type: String, required: true },
+	rooms: { type: Array },
 })
 
-const User = model('User', userModel)
+userSchema.pre('save', async function (next) {
+	const salt = await bcrypt.genSalt(16)
+	const hash = await bcrypt.hash(this.password, salt)
+
+	this.password = hash
+
+	next()
+})
+
+const secretOrKey = process.env.JWT_SECRET_DEV as string
+
+userSchema.methods.generateJWT = function () {
+	const token = jwt.sign(
+		{
+			expiresIn: '12h',
+			id: this._id,
+			provider: this.provider,
+		},
+		secretOrKey
+	)
+
+	return token
+}
+
+const User = model<IUser, UserModel>('User', userSchema)
 
 export default User
