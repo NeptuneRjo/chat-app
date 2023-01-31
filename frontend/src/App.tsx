a62baead74a83904a145ce6b4c2b49bf9ef6e12a
NeptuneRjo <rjo9000@outlook.com>
2023-01-30 19:12:52 -0500

    move hashrouter to index

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
-import { Menu, Room } from './Containers'
+import { Menu, Room, Login, Register } from './Containers'
 import { UserInterface } from './types'
 import { io } from 'socket.io-client'
+import { useNavigate } from 'react-router-dom'
 import { getAndSet } from './Global/utils'
 import { getUser } from './Api'
 
@@ -15,30 +16,59 @@ function App() {
 	})
 
 	const [appError, setAppError] = useState<unknown | null>(null)
-	const [user, setUser] = useState<undefined | UserInterface>(undefined)
+	const [user, setUser] = useState<null | UserInterface>(null)
+	const [token, setToken] = useState<string>('')
+	const [rooms, setRooms] = useState<any>()
+
+	const navigate = useNavigate()
 
 	useEffect(() => {
-		;(async () => {
-			const { data, error } = await getAndSet(getUser)
-			console.log('get user')
+		const sessionToken = window.sessionStorage.getItem('userToken')
 
-			data ? setUser(data) : setAppError(error)
+		;(async () => {
+			// const { data, error } = await getAndSet(getUser)
+			// console.log('get user')
+			// data ? setUser(data) : setAppError(error)
+			if (sessionToken) {
+				const response = await getUser(sessionToken)
+				const json = await response.json()
+
+				if (response.ok) {
+					setToken(sessionToken)
+
+					setUser(json.data)
+
+					json.data.rooms.forEach((room: any) => {
+						setRooms([...rooms, room])
+					})
+				}
+			}
+
+			if (!user) {
+				navigate('/user/login')
+			}
 		})()
-	}, [])
+	}, [user])
 
 	return (
-		<HashRouter>
-			<div className='app-main bg-dark'>
-				<Menu user={user} setUser={setUser} />
-				<Routes>
-					<Route
-						path='/chat/:id'
-						element={<Room user={user} socket={socket} />}
-					/>
-					<Route path='/' element={<Navigate to='/chat/room-1' />} />
-				</Routes>
-			</div>
-		</HashRouter>
+		<div className='app-main bg-dark'>
+			<Menu user={user} setUser={setUser} />
+			<Routes>
+				<Route
+					path='/chat/:id'
+					element={<Room user={user} socket={socket} />}
+				/>
+				<Route
+					path='/user/login'
+					element={<Login setToken={setToken} setUser={setUser} />}
+				/>
+				<Route
+					path='/user/register'
+					element={<Register setToken={setToken} setUser={setUser} />}
+				/>
+				<Route path='/' element={<Navigate to='/chat/room-1' />} />
+			</Routes>
+		</div>
 	)
 }
 
